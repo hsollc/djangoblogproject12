1bee4e5f2399365216394667f80c51a82ab2fe87
sung youl kim <hsollclaw@gmail.com>
2022-10-23 04:53:25 +0900

    Initial commit

--- single_pages/urls.py
+++ single_pages/urls.py
@@ -4,7 +4,6 @@ from . import views
 urlpatterns = [
     path('golf/', views.golf),
     path('art/', views.art),
-    path('cultureheadline/', views.cultureheadline),
     path('hsollc/', views.hsollc),
     path('', views.landing),
 ]
--- single_pages/views.py
+++ single_pages/views.py
@@ -29,50 +29,6 @@ import time
 from newsapi import NewsApiClient
 os.environ["DJANGO_ALLOW_ASYNC_UNSAFE"] = "True"
 
-
-from django.shortcuts import render
-from newsapi import NewsApiClient
-from datetime import datetime, timedelta
-
-def cultureheadline(request):
-    newsapi = NewsApiClient(api_key="c66416a0443445a896fe5e0eff1b1bc5")
-    # topheadlines = newsapi.get_top_headlines(sources='bbc-news')
-    now = datetime.now()
-    before_one_day =  now - timedelta(days=25)
-
-    all_articles = newsapi.get_everything(q='art',
-                                          # sources='bbc-news,the-verge',
-                                          # domains='bbc.co.uk,techcrunch.com',
-                                          from_param=before_one_day,
-                                          to=now,
-                                          language='en',
-                                          sort_by='relevancy',
-                                          # page=1
-                                          )
-
-    # articles = topheadlines['articles']
-    articles = all_articles['articles']
-
-    desc = []
-    news = []
-    img = []
-    url = []
-
-    for i in range(len(articles)):
-        if i < 10:
-            myarticles = articles[i]
-
-            news.append(myarticles['title'])
-            desc.append(myarticles['description'])
-            img.append(myarticles['urlToImage'])
-            url.append(myarticles['url'])
-
-    mylist = zip(news, desc, img, url)
-
-
-    return render(request, 'single_pages/cultureheadline.html', context={"mylist":mylist})
-
-
 def art(request):
     return render(
         request,
